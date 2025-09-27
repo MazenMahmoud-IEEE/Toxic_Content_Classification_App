3130078ad4d431eee55947ff350d883ed6a6c571
Mazen Mahmoud <mazenmahmoud675@gmail.com>
2025-09-27 11:35:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from peft import PeftModel
 # Load fine-tuned model + tokenizer
 # -------------------------
 BASE_MODEL = "distilroberta-base"  
-BEST_CHECKPOINT = r"C:\Projects\Cellula_Internship\NLP\Task2\toxicity_roberta_lora_out\checkpoint-66"
+BEST_CHECKPOINT = ".\checkpoint-66"
 
 # Custom label mapping (from your LabelEncoder)
 id2label = {
@@ -107,3 +107,4 @@ elif option == "Image":
 if st.checkbox("📂 View Database"):
     df = pd.read_csv(DB_FILE)
     st.dataframe(df)
+
